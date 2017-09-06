6a0f49610a3d85c08b434a7de0853fd04e6c3e9d
eugene.toperman <eugene@insureon.com>
2017-09-06 13:30:12 -0500

    CSharp7 solution added

---
+++ CSharp7/CSharp7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharp7
+{
+    using System.Diagnostics;
+    using System.Drawing;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void PrintCoordinates(Point p)
+        {
+            int x, y;   //Have to "predeclare"
+            GetCoordinates(p, out x, out y);
+            Debug.WriteLine( $"({x}, {y})" );
+        }
+
+        public void GetCoordinates(Point p, out int x, out int y)
+        {
+            x = 1;
+            y = 1;
+        }
+    }
+}
